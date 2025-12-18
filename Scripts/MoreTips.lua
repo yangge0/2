4b633b7b7594a33734057ded6a1076c1c1d8676d
lilong <d7xxhsrx2w@privaterelay.appleid.com>
2025-12-18 14:29:47 +0800

    Add files via upload

---
+++ Scripts/MoreTips.lua
@@ -0,0 +1,17 @@
+﻿local MoreTips = GameMain:GetMod("MoreTips");
+
+function MoreTips:OnEnter()
+	CS.Wnd_Help.Instance:Show();
+	CS.Wnd_Help.Instance:Hide();
+	CS.Wnd_Help.Instance.contentPane.m_Text.m_title.onClickLink:Set(MoreTips.onClickLink); 
+end
+
+function MoreTips.onClickLink(Context)
+	local link = Context.data;
+	if string.find(link,"http://") or string.find(link,"https://") then
+		CS.UnityEngine.Application.OpenURL(Context.data)
+	end
+	if string.find(link,"tencent://") and CS.System.Diagnostics.Process.GetProcessesByName("QQ").Length then
+		CS.UnityEngine.Application.OpenURL(Context.data)
+	end
+end
